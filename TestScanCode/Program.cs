3be0717b870baeb6cefeea50a7e92843519737d6
idem.dev <idem.dev@gotrustid.com>
2024-01-29 10:29:30 +0800

    initial commit

--- TestScanCode/Program.cs
+++ TestScanCode/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter ESN:");
+
+        string userInput = Console.ReadLine();
+
+        string ESN_len = string.Format("{0:X2}", userInput.Length);
+        string convert_ESN = string.Empty;
+        foreach (var c in userInput) convert_ESN = convert_ESN + string.Format("{0:X2}", (int)c);
+
+        Console.WriteLine($"ESN：{userInput}");
+        Console.WriteLine($"Converted ESN：{ESN_len}{convert_ESN}");
+
+        Console.ReadKey();
+    }
+}
